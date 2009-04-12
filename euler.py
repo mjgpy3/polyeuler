8af3e48a2c7ce41dfe3d8549f63f5a0cdcb78121
Dustin J. Mitchell <dustin@v.igoro.us>
2009-04-12 10:37:18 -0400

    add problem 18

--- euler.py
+++ euler.py
@@ -467,6 +467,83 @@ def soln():
 
     return sum([ len(word(n)) for n in range(1, 1001) ])
 
+@problem(18)
+@answer(1074)
+def soln():
+    """
+    By starting at the top of the triangle below and moving to adjacent numbers
+    on the row below, the maximum total from top to bottom is 23.
+
+    3
+    7 5
+    2 4 6
+    8 5 9 3
+
+    That is, 3 + 7 + 4 + 9 = 23.
+
+    Find the maximum total from top to bottom of the triangle below:
+
+    75
+    95 64
+    17 47 82
+    18 35 87 10
+    20 04 82 47 65
+    19 01 23 75 03 34
+    88 02 77 73 07 63 67
+    99 65 04 28 06 16 70 92
+    41 41 26 56 83 40 80 70 33
+    41 48 72 33 47 32 37 16 94 29
+    53 71 44 65 25 43 91 52 97 51 14
+    70 11 33 28 77 73 17 78 39 68 17 57
+    91 71 52 38 17 14 91 43 58 50 27 29 48
+    63 66 04 68 89 53 67 30 73 16 69 87 40 31
+    04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+    """
+
+    triangle_rows = [
+        [75],
+        [95,64],
+        [17,47,82],
+        [18,35,87,10],
+        [20,4,82,47,65],
+        [19,1,23,75,3,34],
+        [88,2,77,73,7,63,67],
+        [99,65,4,28,6,16,70,92],
+        [41,41,26,56,83,40,80,70,33],
+        [41,48,72,33,47,32,37,16,94,29],
+        [53,71,44,65,25,43,91,52,97,51,14],
+        [70,11,33,28,77,73,17,78,39,68,17,57],
+        [91,71,52,38,17,14,91,43,58,50,27,29,48],
+        [63,66,4,68,89,53,67,30,73,16,69,87,40,31],
+        [4,62,98,27,23,9,70,98,73,93,38,53,60,4,23],
+        ]
+
+    # convert the triangle into an efficient rooted, directed graph, such that
+    # any path from the root terminates at the bottom of the triangle, and all
+    # paths through the triangle are represented in the graph.
+    def mkgraphs(rows):
+        if len(rows) == 1:
+            return [ (e, None, None) for e in rows[0] ]
+        subgraphs = mkgraphs(rows[1:])
+        row = rows[0]
+        return [ (row[i], subgraphs[i], subgraphs[i+1]) for i in range(len(row)) ]
+    triangle = mkgraphs(triangle_rows)[0]
+
+    # now just do a recursive search
+    def greatestpath(graphroot):
+        e, leftsub, rightsub = graphroot
+        if leftsub is None or rightsub is None:
+            return [e]
+        leftpath = greatestpath(leftsub)
+        rightpath = greatestpath(rightsub)
+        if sum(leftpath) > sum(rightpath):
+            return [ e ] + leftpath
+        else:
+            return [ e ] + rightpath
+
+    gp = greatestpath(triangle)
+    return sum(gp)
+
 ##
 # runner
 
