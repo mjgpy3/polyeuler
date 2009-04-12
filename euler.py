06ae5663f166f5e59aac48887f5f48b606f5f9a9
Dustin J. Mitchell <dustin@v.igoro.us>
2009-04-11 23:48:36 -0400

    add problem 14

--- euler.py
+++ euler.py
@@ -343,6 +343,48 @@ def soln():
     s = str(sum(nums))
     return int(s[:10])
 
+
+@problem(14)
+@answer(837799)
+def soln():
+    """
+    The following iterative sequence is defined for the set of positive integers:
+
+    n -> n/2 (n is even)
+    n -> 3n + 1 (n is odd)
+
+    Using the rule above and starting with 13, we generate the following sequence:
+
+    13  40  20  10  5  16  8  4  2  1
+
+    It can be seen that this sequence (starting at 13 and finishing at 1)
+    contains 10 terms. Although it has not been proved yet (Collatz Problem),
+    it is thought that all starting numbers finish at 1.
+
+    Which starting number, under one million, produces the longest chain?
+
+    NOTE: Once the chain starts the terms are allowed to go above one million.
+    """
+    lens = { 1 : 1 }
+    def getlen(i):
+        if i in lens: return lens[i]
+        if i & 1:
+            l = getlen(3*i+1)+1
+        else:
+            l = getlen(i/2)+1
+        lens[i] = l
+        return l
+
+    maxlen = 0
+    biggest_start = 0
+    for i in xrange(1,1000000):
+        newlen = getlen(i)
+        if newlen > maxlen:
+            biggest_start = i
+            maxlen = newlen
+
+    return biggest_start
+
 ##
 # runner
 
